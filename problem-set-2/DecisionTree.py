81acee751a97942f2101fff1186f7727c4a5c808
Aaron Karp <aaronkarp123@gmail.com>
2015-04-29 23:30:55 -0500

    Updated DecisionTree
    Lots is unfinished, but every line that should be filled in has
    comments indicating that

--- problem-set-2/DecisionTree.py
+++ problem-set-2/DecisionTree.py
@@ -1,3 +1,5 @@
+import math
+
 class DecisionTree:
     """Creates and stores a decision tree
 
@@ -30,8 +32,50 @@ class DecisionTree:
     	Returns:
     		tree DecTree: the filled decision tree
     	"""
-    	#see lectures
-    	pass
+      if not examples:
+        return None
+      tempClassification = #first example's classification
+      ifTrue = true;
+      if sameClass(examples)
+        return tempClassification
+      if not attributes
+        return mode(examples)
+      bestAtt = chooseAttribute(examples, attributes)
+      #TREE + NEW DT WITH ROOT TEST bestAtt
+
+      #for the following for-loop, check attribute-type to determine the type of split to check for
+
+    def mode(examples):
+      """Most common value
+
+      Returns:
+        number val: most common binary output
+      """
+      num0 = 0
+      num1 = 0
+      for element examples:
+        if:#classification is 0
+          num0 += 1
+        else:
+         if:#classifcation is 1
+          num1 += 1
+
+      if num0>num1
+        return 0
+      return 1
+
+    def sameClass(examples):
+      """If all examples have same classification
+
+      Returns:
+        boolean yes/no: if all examples are the same class.
+      """
+      tempClassification = #first example's classification
+      for element examples:
+        if:#this classification != tempClassification
+          return False
+      return True
+
 
    	def chooseAttribute(examples, attributes):
    		"""Chooses best attribute to split on
@@ -49,7 +93,9 @@ class DecisionTree:
    		Returns:
    			number entropy: entropy of the attribute at the split
    		"""
-   		#dunnomate
+   		#1) Look through every example and tally the ones that correctly fall on either side of the split
+      #2) Calculate the proportion of these to the total number (ie proportion of correct classifications)
+      #3) entropy(S) = -p1log2(p1) - p2log2(p2)
    		pass
 
    	def prune(tree):
