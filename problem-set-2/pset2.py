7eba1f3be1563189fdd9787cc6f1870dff011b59
Aaron Karp <aaronkarp123@gmail.com>
2015-05-01 20:27:36 -0500

    Updated- No Errors!
    Fixed ~500 errors. Yay.

--- problem-set-2/pset2.py
+++ problem-set-2/pset2.py
@@ -1,12 +1,14 @@
 import CSVReader as csv
 import MetaReader as meta
+import DecisionTree as DT
 
 training_set = csv.CSVReader("./data/btrain.csv")
 training_header, training_data = training_set.readFile()
 
-print training_header
 # print training_data
 
 meta_reader = meta.MetaReader("./data/bmeta.csv")
 meta_data = meta_reader.parseMetaData()
-print meta_data["winpercent"]["type"]
+
+thisTree = DT.DecisionTree(training_data, meta_data)
+tree = thisTree.treeMaker()
